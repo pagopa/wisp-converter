3dd6be06287f56577cdedfa65a2dcc0a6bd379a7
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2024-04-03 08:58:08 +0200

    [NOD-781] fix: completed OpenAPI's description for /redirect API

--- src/main/java/it/gov/pagopa/wispconverter/controller/RedirectController.java
+++ src/main/java/it/gov/pagopa/wispconverter/controller/RedirectController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import it.gov.pagopa.wispconverter.exception.AppErrorCodeMessageEnum;
 import it.gov.pagopa.wispconverter.exception.AppException;
 import it.gov.pagopa.wispconverter.service.ConverterService;
+import it.gov.pagopa.wispconverter.util.CommonUtility;
 import it.gov.pagopa.wispconverter.util.openapi.OpenAPITableMetadata;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotBlank;
@@ -34,30 +35,30 @@ public class RedirectController {
 
     private final ConverterService converterService;
 
-    @Operation(summary = "", description = "", security = {@SecurityRequirement(name = "ApiKey")}, tags = {"Redirect"})
+    @Operation(summary = "Redirect payment from WISP to Checkout", description = "Redirecting the payment flow from WISP to Checkout. In order to do so, the NodoInviaRPT and NodoInviaCarrelloRPT requests will be converted in NMU payments and handled by GPD system.", security = {@SecurityRequirement(name = "ApiKey")}, tags = {"Redirect"})
     @ApiResponses(value = {
             @ApiResponse(responseCode = "302", description = "Redirect to Checkout service.", content = @Content(schema = @Schema())),
             @ApiResponse(responseCode = "422", description = "Unprocessable request. Returns a static page with the error code.", content = @Content(schema = @Schema()))
     })
-    @OpenAPITableMetadata
+    @OpenAPITableMetadata(external = false, authentication = OpenAPITableMetadata.APISecurityMode.APIKEY, readWriteIntense = OpenAPITableMetadata.ReadWrite.BOTH)
     @GetMapping
-    public ModelAndView redirect(@Parameter(description = "", example = "identificativoIntermediarioPA_sessionId")
+    public ModelAndView redirect(@Parameter(description = "The identifier of the payment, referenced by session and creditor institution broker.", example = "identificativoIntermediarioPA_sessionId")
                                  @NotBlank @RequestParam("sessionId") String sessionId,
                                  HttpServletResponse response) throws IOException {
 
-        String errorCode;
+        AppErrorCodeMessageEnum errorCode;
         try {
             response.sendRedirect(converterService.convert(sessionId));
             return null;
         } catch (AppException e) {
-            errorCode = String.valueOf(e.getError().getCode());
+            errorCode = e.getError();
         } catch (Exception e) {
-            errorCode = String.valueOf(AppErrorCodeMessageEnum.GENERIC_ERROR.getCode());
+            errorCode = AppErrorCodeMessageEnum.GENERIC_ERROR;
         }
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setStatus(HttpStatusCode.valueOf(422));
         modelAndView.setViewName("error.html");
-        modelAndView.addObject("error", "WIC-" + errorCode);
+        modelAndView.addObject("error", CommonUtility.getAppCode(errorCode));
         return modelAndView;
     }
 }
