9c5f7e48b08be0353c1a739ba13709a85c4c8e1b
maxsca <130107847+maxsca@users.noreply.github.com>
2024-04-08 18:04:09 +0200

    NOD-784 re

--- src/main/java/it/gov/pagopa/wispconverter/service/model/re/EsitoEnum.java
+++ src/main/java/it/gov/pagopa/wispconverter/service/model/re/EsitoEnum.java
@@ -1,5 +1,5 @@
 package it.gov.pagopa.wispconverter.service.model.re;
 
 public enum EsitoEnum {
-    INVIATA, INVIATA_KO, RICEVUTA, RICEVUTA_KO, CAMBIO_STATO;
+    INVIATA, INVIATA_KO, RICEVUTA, RICEVUTA_KO, NO_RICEVUTA, CAMBIO_STATO;
 }
