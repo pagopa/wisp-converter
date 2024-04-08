4ee1f288c858925b6bd855666d1c707995ce5c9b
Lorenzo Catalano <lorenzo.catalano@tasgroup.eu>
2024-04-08 17:56:56 +0200

    added unit test

--- src/test/java/it/gov/pagopa/wispconverter/RtTest.java
+++ src/test/java/it/gov/pagopa/wispconverter/RtTest.java
@@ -5,7 +5,6 @@ import static org.mockito.Mockito.*;
 
 import com.azure.cosmos.CosmosClientBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import it.gov.pagopa.gen.wispconverter.client.iuvgenerator.model.IuvGenerationModelResponseDto;
 import it.gov.pagopa.wispconverter.controller.model.ReceiptRequest;
 import it.gov.pagopa.wispconverter.repository.RPTRequestRepository;
 import it.gov.pagopa.wispconverter.service.ConfigCacheService;
@@ -23,8 +22,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.metrics.ApplicationStartup;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
@@ -76,30 +73,7 @@ class RtTest {
     void success_positive() throws Exception {
         String station = "mystation";
         TestUtils.setMock(cacheClient,ResponseEntity.ok().body(TestUtils.configData(station)));
-
         org.springframework.test.util.ReflectionTestUtils.setField(configCacheService, "configCacheClient",cacheClient);
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("location","locationheader");
-        TestUtils.setMock(checkoutClient, ResponseEntity.status(HttpStatus.FOUND).headers(headers).build());
-
-        IuvGenerationModelResponseDto iuvGenerationModelResponseDto = new IuvGenerationModelResponseDto();
-        iuvGenerationModelResponseDto.setIuv("00000000");
-        TestUtils.setMock(iuveneratorClient,ResponseEntity.ok().body(iuvGenerationModelResponseDto));
-        TestUtils.setMock(gpdClient,ResponseEntity.ok().build());
-        TestUtils.setMock(decouplerCachingClient,ResponseEntity.ok().build());
-//        when(rptRequestRepository.findById(any())).thenReturn(
-//                Optional.of(
-//                        RPTRequestEntity.builder().primitive("nodoInviaRPT")
-//                                .payload(
-//                                        new String(Base64.getEncoder().encode(zip(
-//                                                getRptPayload(false,station,"100.00",null).getBytes(StandardCharsets.UTF_8))),StandardCharsets.UTF_8)
-//                                ).build()
-//                )
-//        );
-//        when(redisSimpleTemplate.opsForValue()).thenReturn(mock(ValueOperations.class));
-
-
-
         mvc.perform(MockMvcRequestBuilders.post("/receipt/ok")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
@@ -110,49 +84,16 @@ class RtTest {
                             assertNotNull(result);
                             assertNotNull(result.getResponse());
                         });
-
-//        verify(checkoutClient,times(1)).invokeAPI(any(),any(),any(),any(),any(),any(),any(),any(),any(),any(),any(),any());
-//        verify(iuveneratorClient,times(1)).invokeAPI(any(),any(),any(),any(),any(),any(),any(),any(),any(),any(),any(),any());
-//        verify(gpdClient,times(1)).invokeAPI(any(),any(),any(),any(),any(),any(),any(),any(),any(),any(),any(),any());
-//        verify(decouplerCachingClient,times(1)).invokeAPI(any(),any(),any(),any(),any(),any(),any(),any(),any(),any(),any(),any());
-//
-//        ArgumentCaptor<Object> argument = ArgumentCaptor.forClass(Object.class);
-//        verify(gpdClient).invokeAPI(any(),any(),any(),any(),argument.capture(),any(),any(),any(),any(),any(),any(),any());
-//        MultiplePaymentPositionModelDto value = (MultiplePaymentPositionModelDto) argument.getValue();
-//        assertEquals(1, value.getPaymentPositions().size());
-//        assertEquals("TTTTTT11T11T123T", value.getPaymentPositions().get(0).getFiscalCode());
     }
 
     @Test
     void success_negative() throws Exception {
         String station = "mystation";
         TestUtils.setMock(cacheClient,ResponseEntity.ok().body(TestUtils.configData(station)));
-
         org.springframework.test.util.ReflectionTestUtils.setField(configCacheService, "configCacheClient",cacheClient);
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("location","locationheader");
-        TestUtils.setMock(checkoutClient, ResponseEntity.status(HttpStatus.FOUND).headers(headers).build());
-
-        IuvGenerationModelResponseDto iuvGenerationModelResponseDto = new IuvGenerationModelResponseDto();
-        iuvGenerationModelResponseDto.setIuv("00000000");
-        TestUtils.setMock(iuveneratorClient,ResponseEntity.ok().body(iuvGenerationModelResponseDto));
-        TestUtils.setMock(gpdClient,ResponseEntity.ok().build());
-        TestUtils.setMock(decouplerCachingClient,ResponseEntity.ok().build());
-//        when(rptRequestRepository.findById(any())).thenReturn(
-//                Optional.of(
-//                        RPTRequestEntity.builder().primitive("nodoInviaRPT")
-//                                .payload(
-//                                        new String(Base64.getEncoder().encode(zip(
-//                                                getRptPayload(false,station,"100.00",null).getBytes(StandardCharsets.UTF_8))),StandardCharsets.UTF_8)
-//                                ).build()
-//                )
-//        );
-//        when(redisSimpleTemplate.opsForValue()).thenReturn(mock(ValueOperations.class));
-
         ReceiptDto[] receiptDtos = {
                 new ReceiptDto("", "", "")
         };
-
         mvc.perform(MockMvcRequestBuilders.post("/receipt/ko")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
@@ -163,17 +104,6 @@ class RtTest {
                             assertNotNull(result);
                             assertNotNull(result.getResponse());
                         });
-
-//        verify(checkoutClient,times(1)).invokeAPI(any(),any(),any(),any(),any(),any(),any(),any(),any(),any(),any(),any());
-//        verify(iuveneratorClient,times(1)).invokeAPI(any(),any(),any(),any(),any(),any(),any(),any(),any(),any(),any(),any());
-//        verify(gpdClient,times(1)).invokeAPI(any(),any(),any(),any(),any(),any(),any(),any(),any(),any(),any(),any());
-//        verify(decouplerCachingClient,times(1)).invokeAPI(any(),any(),any(),any(),any(),any(),any(),any(),any(),any(),any(),any());
-//
-//        ArgumentCaptor<Object> argument = ArgumentCaptor.forClass(Object.class);
-//        verify(gpdClient).invokeAPI(any(),any(),any(),any(),argument.capture(),any(),any(),any(),any(),any(),any(),any());
-//        MultiplePaymentPositionModelDto value = (MultiplePaymentPositionModelDto) argument.getValue();
-//        assertEquals(1, value.getPaymentPositions().size());
-//        assertEquals("TTTTTT11T11T123T", value.getPaymentPositions().get(0).getFiscalCode());
     }
      
 }
