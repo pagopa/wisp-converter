0a820c9227120a40ca2be337b20f7c988ddd0936
AngeloCaporaso <angelo.caporaso@pagopa.it>
2024-06-19 16:26:47 +0200

    test: Extract ReceiptTimerController class and add ReceiptTimerTest

--- src/main/java/it/gov/pagopa/wispconverter/controller/ReceiptController.java
+++ src/main/java/it/gov/pagopa/wispconverter/controller/ReceiptController.java
@@ -8,9 +8,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import it.gov.pagopa.wispconverter.controller.model.ReceiptRequest;
-import it.gov.pagopa.wispconverter.controller.model.ReceiptTimerRequest;
 import it.gov.pagopa.wispconverter.service.ReceiptService;
-import it.gov.pagopa.wispconverter.service.ReceiptTimerService;
 import it.gov.pagopa.wispconverter.util.Trace;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,7 +17,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequestMapping("/receipt")
@@ -31,11 +28,7 @@ public class ReceiptController {
 
     private static final String BP_RECEIPT_OK = "receipt-ok";
     private static final String BP_RECEIPT_KO = "receipt-ko";
-    private static final String BP_TIMER_SET = "timer-set";
-    private static final String BP_TIMER_DELETE = "timer-delete";
     private final ReceiptService receiptService;
-
-    private final ReceiptTimerService receiptTimerService;
 
     @Operation(summary = "", description = "", security = {@SecurityRequirement(name = "ApiKey")}, tags = {"Receipt"})
     @ApiResponses(value = {
@@ -65,35 +58,5 @@ public class ReceiptController {
     public void receiptOk(@RequestBody ReceiptRequest request) throws IOException {
 
         receiptService.paaInviaRTOk(request.getContent());
-    }
-
-    @Operation(summary = "createTimer", description = "Create a timer linked with paymentToken and receipt data", security = {@SecurityRequirement(name = "ApiKey")}, tags = {"ReceiptTimer"})
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully paymentToken expiration timer created", content = @Content(schema = @Schema()))
-    })
-    @PostMapping(
-            value = "/timer",
-            produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = MediaType.APPLICATION_JSON_VALUE
-    )
-    @Trace(businessProcess = BP_TIMER_SET, reEnabled = true)
-    public void createTimer(@RequestBody ReceiptTimerRequest request) {
-        log.info("Set Timer arrived: " + request.toString());
-        receiptTimerService.sendMessage(request);
-    }
-
-    @Operation(summary = "deleteTimer", description = "Delete a timer by paymentToken", security = {@SecurityRequirement(name = "ApiKey")}, tags = {"ReceiptTimer"})
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully paymentToken expiration timer deleted", content = @Content(schema = @Schema()))
-    })
-    @DeleteMapping(
-            value = "/timer",
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    @Trace(businessProcess = BP_TIMER_DELETE, reEnabled = true)
-    public void deleteTimer(@RequestParam List<String> paymentTokens) {
-        log.info("Delete Timer arrived: " + paymentTokens);
-        receiptTimerService.cancelScheduledMessage(paymentTokens);
-
     }
 }
---
+++ src/main/java/it/gov/pagopa/wispconverter/controller/ReceiptTimerController.java
@@ -0,0 +1,61 @@
+package it.gov.pagopa.wispconverter.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import it.gov.pagopa.wispconverter.controller.model.ReceiptTimerRequest;
+import it.gov.pagopa.wispconverter.service.ReceiptTimerService;
+import it.gov.pagopa.wispconverter.util.Trace;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/receipt")
+@Validated
+@RequiredArgsConstructor
+@Tag(name = "ReceiptTimer", description = "Create and Delete payment token timer")
+@Slf4j
+public class ReceiptTimerController {
+    private static final String BP_TIMER_SET = "timer-set";
+    private static final String BP_TIMER_DELETE = "timer-delete";
+
+    private final ReceiptTimerService receiptTimerService;
+
+    @Operation(summary = "createTimer", description = "Create a timer linked with paymentToken and receipt data", security = {@SecurityRequirement(name = "ApiKey")}, tags = {"ReceiptTimer"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully paymentToken expiration timer created", content = @Content(schema = @Schema()))
+    })
+    @PostMapping(
+            value = "/timer",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Trace(businessProcess = BP_TIMER_SET, reEnabled = true)
+    public void createTimer(@RequestBody ReceiptTimerRequest request) {
+        log.info("Set Timer arrived: " + request.toString());
+        receiptTimerService.sendMessage(request);
+    }
+
+    @Operation(summary = "deleteTimer", description = "Delete a timer by paymentToken", security = {@SecurityRequirement(name = "ApiKey")}, tags = {"ReceiptTimer"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully paymentToken expiration timer deleted", content = @Content(schema = @Schema()))
+    })
+    @DeleteMapping(
+            value = "/timer",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Trace(businessProcess = BP_TIMER_DELETE, reEnabled = true)
+    public void deleteTimer(@RequestParam List<String> paymentTokens) {
+        log.info("Delete Timer arrived: " + paymentTokens);
+        receiptTimerService.cancelScheduledMessage(paymentTokens);
+
+    }
+}
