89597de17b6816c691d4c2414e6143b451919f54
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2024-06-26 16:11:43 +0200

    fix: changed station check re-adding segregation code analysis

--- src/main/java/it/gov/pagopa/wispconverter/service/DebtPositionService.java
+++ src/main/java/it/gov/pagopa/wispconverter/service/DebtPositionService.java
@@ -390,7 +390,7 @@ public class DebtPositionService {
         try {
 
             // validate the station, checking if exists one with the required segregation code and, if is onboarded on GPD, has the correct primitive version
-            CommonUtility.checkStationValidity(configCacheService, sessionData);
+            CommonUtility.checkStationValidity(configCacheService, sessionData, CommonUtility.getSinglePaymentOption(paymentPositionFromGPD).getNav());
 
             // merge the information of extracted payment position with the data from existing payment position, retrieved from GPD
             PaymentPositionModelDto updatedPaymentPosition = updateExtractedPaymentPositionWithExistingData(iuv, sessionData, paymentPositionFromGPD, extractedPaymentPosition);
@@ -467,7 +467,7 @@ public class DebtPositionService {
         }
 
         // validate the station, checking if exists one with the required segregation code and, if is onboarded on GPD, has the correct primitive version
-        CommonUtility.checkStationValidity(configCacheService, sessionData);
+        CommonUtility.checkStationValidity(configCacheService, sessionData, nav);
 
         // update the payment option to be used in bulk insert with the newly generated NAV code
         List<PaymentOptionModelDto> paymentOptions = extractedPaymentPosition.getPaymentOption();
