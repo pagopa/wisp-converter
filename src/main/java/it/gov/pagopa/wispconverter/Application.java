9312c07b8fba44e1657bfd5c4d6d33a6f86e6518
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2024-07-22 10:36:39 +0200

    [PAGOPA-1964] fix: reverted async endpoint

--- src/main/java/it/gov/pagopa/wispconverter/Application.java
+++ src/main/java/it/gov/pagopa/wispconverter/Application.java
@@ -7,10 +7,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class Application {
 
     @Value("${azure.sb.wisp-paainviart-queue.connectionString}")
