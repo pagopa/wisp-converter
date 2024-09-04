0fc46c995feea908d903884d355fc0ae252e1501
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2024-09-04 09:41:51 +0200

    [PAGOPA-2086] fix: resolved possible Cross-Site Request Forgery

--- src/main/java/it/gov/pagopa/wispconverter/controller/StaticPagesController.java
+++ src/main/java/it/gov/pagopa/wispconverter/controller/StaticPagesController.java
@@ -1,13 +1,14 @@
 package it.gov.pagopa.wispconverter.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller()
 @RequestMapping("/static")
 public class StaticPagesController {
 
-    @RequestMapping("/error")
+    @GetMapping(path = "/error")
     public String error() {
         return "error";
     }
