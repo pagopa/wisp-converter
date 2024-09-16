eee887d9d97151965ac008d9b52ef575e7fe8fa6
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2024-09-16 10:54:54 +0200

    PAGOPA-2091 fix toString in messageRequest

--- src/main/java/it/gov/pagopa/wispconverter/controller/model/RPTTimerRequest.java
+++ src/main/java/it/gov/pagopa/wispconverter/controller/model/RPTTimerRequest.java
@@ -1,16 +1,21 @@
 package it.gov.pagopa.wispconverter.controller.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.google.gson.Gson;
 import lombok.*;
 
 @Data
 @Builder(toBuilder = true)
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RPTTimerRequest {
 
     private String sessionId;
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
+    }
 
 }
