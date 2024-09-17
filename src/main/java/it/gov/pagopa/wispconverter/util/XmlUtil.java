c7496a9cf4379fa6755179a0df633a223f24d7e1
Andrea De Rinaldis <117269497+andrea-deri@users.noreply.github.com>
2024-09-17 16:17:18 +0200

    [PAGOPA-2132] fix: resolved wrong formattation for RT dates (#107)
    Co-authored-by: pagopa-github-bot <github-bot@pagopa.it>

--- src/main/java/it/gov/pagopa/wispconverter/util/XmlUtil.java
+++ src/main/java/it/gov/pagopa/wispconverter/util/XmlUtil.java
@@ -4,6 +4,7 @@ import it.gov.pagopa.wispconverter.exception.AppErrorCodeMessageEnum;
 import it.gov.pagopa.wispconverter.exception.AppException;
 
 import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeConstants;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 import java.math.BigDecimal;
@@ -14,15 +15,16 @@ import java.util.GregorianCalendar;
 public class XmlUtil {
 
     public static XMLGregorianCalendar toXMLGregoirianCalendar(Instant instant) {
-        GregorianCalendar gc = new GregorianCalendar();
-        gc.setTimeInMillis(instant.toEpochMilli());
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTimeInMillis(instant.toEpochMilli());
         try {
-            return DatatypeFactory.newInstance().newXMLGregorianCalendar(gc);
+            XMLGregorianCalendar xmlGregorianCalendar = DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar);
+            xmlGregorianCalendar.setTimezone(DatatypeConstants.FIELD_UNDEFINED); // removing all references about timezone
+            return xmlGregorianCalendar;
         } catch (DatatypeConfigurationException e) {
             throw new AppException(e, AppErrorCodeMessageEnum.PARSING_INVALID_BODY);
         }
     }
-
 
     public static BigDecimal toBigDecimalWithScale(BigDecimal target, int scale) {
         return target.setScale(scale, RoundingMode.UNNECESSARY);
