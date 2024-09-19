99b0cc6ae120713c42b2427c8efbcb7ebeba416a
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2024-09-19 16:13:10 +0200

    [PAGOPA-2160] fix: resolved bug on wrong sessionID extraction

--- src/main/java/it/gov/pagopa/wispconverter/service/RtRetryComosService.java
+++ src/main/java/it/gov/pagopa/wispconverter/service/RtRetryComosService.java
@@ -7,9 +7,11 @@ import it.gov.pagopa.wispconverter.repository.RTRetryRepository;
 import it.gov.pagopa.wispconverter.repository.model.RTRequestEntity;
 import it.gov.pagopa.wispconverter.repository.model.enumz.InternalStepStatus;
 import it.gov.pagopa.wispconverter.service.model.re.ReEventDto;
+import it.gov.pagopa.wispconverter.util.Constants;
 import it.gov.pagopa.wispconverter.util.ReUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -45,6 +47,7 @@ public class RtRetryComosService {
         RTRequestEntity rtRequestEntity = optRTReqEntity.orElseThrow(() -> new AppException(AppErrorCodeMessageEnum.PERSISTENCE_RT_NOT_FOUND, id, partitionKey));
 
         // generate and save RE event internal for change status
+        MDC.put(Constants.MDC_SESSION_ID, rtRequestEntity.getIdempotencyKey().split("_")[0]);
         generateRE(rtRequestEntity.getPayload());
 
         return rtRequestEntity;
