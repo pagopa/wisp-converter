223d84a40b5ca0dc98a606ed613edd1e3ff1933e
Francesco Cesareo <cesareo.francesco@gmail.com>
2024-10-11 16:35:22 +0200

    added allargs to dead-letter

--- src/main/java/it/gov/pagopa/wispconverter/repository/model/ReceiptDeadLetterEntity.java
+++ src/main/java/it/gov/pagopa/wispconverter/repository/model/ReceiptDeadLetterEntity.java
@@ -1,8 +1,10 @@
 package it.gov.pagopa.wispconverter.repository.model;
 
 import com.azure.spring.data.cosmos.core.mapping.Container;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
@@ -11,6 +13,8 @@ import lombok.experimental.SuperBuilder;
 @ToString
 @EqualsAndHashCode(callSuper=false)
 @SuperBuilder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReceiptDeadLetterEntity extends RTRequestEntity {
 
 }
