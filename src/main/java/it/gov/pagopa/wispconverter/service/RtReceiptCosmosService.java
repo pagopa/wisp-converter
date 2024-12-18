4e3c9a61efe0b35061078e1deb4ed6052f37d09d
Angelo Caporaso <56113767+cap-ang@users.noreply.github.com>
2024-12-18 14:14:20 +0100

    fix: Illegal chars replace

--- src/main/java/it/gov/pagopa/wispconverter/service/RtReceiptCosmosService.java
+++ src/main/java/it/gov/pagopa/wispconverter/service/RtReceiptCosmosService.java
@@ -111,7 +111,7 @@ public class RtReceiptCosmosService {
     }
 
     public Optional<RTEntity> findById(String domainId, String iuv, String ccp) {
-        String id = String.format("%s_%s_%s", domainId, iuv, ccp);
+        String id = getId(domainId, iuv, ccp);
         // Remove illegal characters ['/', '\', '#'] because cannot be used in Resource ID
         return rtRepository.findById(id, new PartitionKey(id));
     }
